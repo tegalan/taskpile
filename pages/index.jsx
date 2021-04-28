2dc9085df05e640c81b5248fe805612cb331ed4f
Sucipto <sucipto@pm.me>
2021-04-28 13:16:48 +0700

    Add timer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
+import { addSeconds, differenceInSeconds, formatDistanceStrict } from 'date-fns'
 import Head from 'next/head'
-import { useEffect, useReducer } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 
 const initialState = {
   tasks: [],
@@ -38,7 +39,7 @@ function reducer (state, action) {
           if (m.active) {
             return {
               ...m,
-              finish: new Date(),
+              finish: (new Date()).getTime(),
               active: false
             }
           }
@@ -48,12 +49,20 @@ function reducer (state, action) {
       } else {
         // Start timer & set active
         target.timers.push({
-          start: new Date(),
+          start: (new Date()).getTime(),
           finish: null,
           active: true,
           isBreak: false
         })
       }
+
+      // Calculate total spend
+      target.elapsed = target.timers.reduce((total, timer) => {
+        if (timer.start && timer.finish) {
+          total += differenceInSeconds(new Date(timer.finish), new Date(timer.start))
+        }
+        return total
+      }, 0)
 
       tasks = [target, ...tasks]
       return {
@@ -69,9 +78,49 @@ function reducer (state, action) {
 
 export default function Home () {
   const [state, dispatch] = useReducer(reducer, initialState, initializer)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const activeTask = state.tasks.find(f => f.id === state.active)
 
   useEffect(() => {
     localStorage.setItem('store', JSON.stringify(state))
+
+    let timerInterval = null
+    if (state.active && activeTask) {
+      const activeTimer = activeTask.timers.find(f => f.active)
+      const endTimer = addSeconds(new Date(activeTimer.start), 25 * 60)
+      const diff = differenceInSeconds(endTimer, new Date())
+      // console.log('time diff', activeTimer.start, diff)
+
+      if (diff > 0 && timerInterval === null) {
+        setMinutes(Math.floor(diff / 60))
+        setSeconds(diff % 60)
+      }
+
+      timerInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1)
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
+            startTimer(activeTask)
+            clearInterval(timerInterval)
+          } else {
+            setMinutes(minutes - 1)
+            setSeconds(59)
+          }
+        }
+      }, 1000)
+    } else {
+      setMinutes(0)
+      setSeconds(0)
+    }
+
+    return () => {
+      if (timerInterval) {
+        clearInterval(timerInterval)
+      }
+    }
   })
 
   function onInputKeydown (e) {
@@ -89,7 +138,12 @@ export default function Home () {
     dispatch({ type: 'remove_task', id: task.id })
   }
 
-  const activeTask = state.tasks.find(f => f.id === state.active)
+  function getElapsed (seconds) {
+    if (!seconds) return ''
+    const end = addSeconds(new Date(0), seconds)
+
+    return formatDistanceStrict(new Date(0), end)
+  }
 
   return (
     <div>
@@ -104,7 +158,7 @@ export default function Home () {
             <div
               id="timer"
               className="flex w-60 h-60 items-center justify-center px-4 py-8 text-6xl font-bold text-gray-800 bg-white rounded-full shadow-sm">
-              25:00
+              {minutes}:{seconds}
             </div>
             <div className="task-title text-lg font-medium text-gray-700 py-2">{activeTask?.name}</div>
           </div>
@@ -116,8 +170,9 @@ export default function Home () {
           </div>
           <div id="task-lists" className="mt-2">
             { state.tasks.map((i) =>
-              <div key={i.id} className="tasks flex justify-between px-3 py-2 bg-white rounded shadow-sm mb-1 group">
-                <div className="px-2">{ i.name }</div>
+              <div key={i.id} className="tasks flex px-3 py-2 bg-white rounded shadow-sm mb-1 group">
+                <div className="px-2 flex-grow">{ i.name }</div>
+                <div className="elapsed">{ getElapsed(i.elapsed) }</div>
                 <div className="task-action items-center justify-center hidden group-hover:flex">
                   <button onClick={() => deleteTask(i)} className="focus:outline-none px-2">
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" className="fill-current text-gray-600 hover:text-red-500 w-4 h-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg>
